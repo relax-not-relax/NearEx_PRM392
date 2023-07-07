a21bf9adfcedf2dc23f5417ac9217410521872d0
anhm-ng <aminhng3012@gmail.com>
2023-07-07 23:16:01 +0700

    update UI

--- app/src/main/java/com/example/nearex_prm392/LoginActivity.java
+++ app/src/main/java/com/example/nearex_prm392/LoginActivity.java
@@ -1,14 +1,28 @@
 package com.example.nearex_prm392;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    AppCompatButton btnBack, btnGoogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnBack = (AppCompatButton) findViewById(R.id.buttonBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, ThirdActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/nearex_prm392/LoginOldCustomer.java
@@ -0,0 +1,14 @@
+package com.example.nearex_prm392;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginOldCustomer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_old_customer);
+    }
+}
